e74567e1a57e9979ace477c3436d658c47be626c
TuetrungNguyen <44383594+TuetrungNguyen@users.noreply.github.com>
2024-11-18 09:08:41 -0800

    Adjusted max width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="h-screen bg-slate-300 flex items-center justify-center">
-      <div className="card w-[18rem] bg-white shadow-outline flex items-center p-4 text-center">
+      <div className="card max-w-[18rem] w-full bg-white shadow-outline flex items-center p-4 text-center">
         <div className="bg-code bg-cover bg-center w-full aspect-square rounded mb-4" />
         <h1 className="text-slate-900 font-outfit-regular font-bold text-xl px-3">
           Improve your front-end skills by building projects
